b3506babbb374a478cdc0d79ce89237410baa85b
Yusuf Gören <35740249+Dunpy@users.noreply.github.com>
2019-07-25 19:55:13 +0300

    space war
    space war

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace deneme2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(14, 440);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(52, 46);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(-2, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(423, 498);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(88, 6);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 16);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 532);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+
+namespace deneme2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Location = new Point(14, 440);
+
+        }
+        int x, y, z, sayac, gidilenyol;
+        int toplamcan = 1;
+        int kalancan=1;
+        Random rnd = new Random();
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            y = pictureBox1.Location.Y;
+            x = pictureBox1.Location.X;
+            if (e.KeyCode == Keys.Right && x < 400)
+            {
+                sagagit();
+            }
+            if (e.KeyCode == Keys.Left && x > 0)
+            {
+                solagit();
+            }
+            pictureBox1.Location = new Point(x, y);
+
+            if (e.KeyCode == Keys.Space)
+            {
+                mermiOlustur();
+
+            }
+
+        }
+        void YeniOyun()
+        {
+            gidilenyol = 0;            
+        }
+        void sagagit()
+        {
+            x += 5;
+        }
+        void solagit()
+        {
+            x -= 5;
+        }
+        private void dusmanOlustur()
+        {
+            z = rnd.Next(0, 420);
+            PictureBox dusman = new PictureBox();
+            dusman.Size = new Size(20, 20);
+            dusman.ImageLocation = "dusman.png";
+            dusman.Location = new Point(z, 0);
+            dusman.SizeMode = PictureBoxSizeMode.StretchImage;
+            dusman.Tag = "dusman";
+            this.Controls.Add(dusman);
+            panel1.Controls.Add(dusman);
+        }
+        private void mermiOlustur()
+        {
+            PictureBox mermi = new PictureBox();
+            mermi.Size = new Size(15,15);
+            mermi.ImageLocation = "füze.png";
+            mermi.Location = new Point(x+14, y - 10);
+            mermi.SizeMode = PictureBoxSizeMode.StretchImage;
+            mermi.Tag = "mermi";
+            this.Controls.Add(mermi);
+            panel1.Controls.Add(mermi);
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            timer1.Start();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+            progressBar1.Value = 100 / toplamcan * kalancan;
+            label1.Text = "Gidilen Yol:" + gidilenyol.ToString() + "m";
+            for (int i = 0; i < panel1.Controls.Count; i++)
+            {
+                PictureBox pb = ((PictureBox)panel1.Controls[i]);
+                if (panel1.Controls[i].Tag == "mermi")
+                {
+                    pb.Location = new Point(pb.Location.X, pb.Location.Y - 15);
+
+                }
+                for (int a = 0; a < panel1.Controls.Count; a++)
+                {
+                    if (panel1.Controls[a].Tag == "dusman")
+                    {
+                        if (pb.Location.X + pb.Width > panel1.Controls[a].Location.X && pb.Location.X + pb.Width < panel1.Controls[a].Location.X + panel1.Controls[a].Width)
+                        {
+                            if (pb.Location.Y + pb.Height > panel1.Controls[a].Location.Y && pb.Location.Y + pb.Height < panel1.Controls[a].Location.Y + panel1.Controls[a].Height)
+                            {
+                                panel1.Controls.RemoveAt(a);
+                                panel1.Controls.Remove(pb);
+
+                            }
+                        }
+                        if(panel1.Controls[a].Location.Y==440 && panel1.Controls[a].Location.Y==y)
+                        {
+                            YeniOyun();
+                        }
+                    }
+
+
+                }
+                if (panel1.Controls[i].Tag == "dusman")
+                {
+                    pb.Location = new Point(pb.Location.X, pb.Location.Y + 8);
+
+                }
+            }
+
+            if (sayac % 10 == 0)
+            {
+                gidilenyol++;
+                dusmanOlustur();
+            }
+            
+        }
+     }
+}
+       
+   
+
